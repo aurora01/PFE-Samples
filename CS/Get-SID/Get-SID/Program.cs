f4416a5aa66b1be19bfa214242ab8317c53a15dc
Alex Su <alex.su@live.com>
2017-08-07 21:24:03 -0700

    Base line

---
+++ CS/Get-SID/Get-SID/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Principal;
+
+
+namespace Get_SID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // set up domain context
+            var userIdentity = System.Security.Principal.WindowsIdentity.GetCurrent();
+            var sid1 = userIdentity.User.Value;
+            var sid2 = userIdentity.User.AccountDomainSid.Value;
+            Console.WriteLine(@"SID=" + sid1);
+            Console.WriteLine(@"AccountDomainSid=" + sid2);
+        }
+    }
+}
